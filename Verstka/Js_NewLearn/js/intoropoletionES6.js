1accfeec58ee29b3006a333e57a5be4cae32eae1
Tagary <vanekov13@mail.ru>
2021-07-26 13:41:07 +0700

    SecondCom

---
+++ Verstka/Js_NewLearn/js/intoropoletionES6.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const category = "toys";
+
+console.log(`https://someurls.com/${category}/5` ); //Только косые ковычки дают интрополяцию
+
+const user = "Ivan";
+
+alert(`Привет, ${user}`); 
